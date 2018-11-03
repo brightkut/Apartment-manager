02ce126bbc410675033d07a4013e6b3dd4d61eaa
Jutinapas Klangcharoenkul <jutinapas@gmail.com>
2018-11-03 14:22:11 +0700

    add template for view and controller and icon images in app

---
+++ src/main/java/Controllers/ControllerTemplate.java
@@ -0,0 +1,43 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerTemplate {
+
+    //ปุ่มของเมนู
+    @FXML
+    private Button feature1Btn;
+    @FXML
+    private Button feature2Btn;
+    @FXML
+    private Button feature4Btn;
+    @FXML
+    private Button feature5Btn;
+
+    //ไปหน้าค้นหาจากเมนู
+    @FXML
+    void handleFeature1Btn(ActionEvent event) {
+
+    }
+
+    //ไปหน้าแจ้งชำระจากเมนู
+    @FXML
+    void handleFeature2Btn(ActionEvent event) {
+
+    }
+
+    //ไปหน้าจัดการห้องจากเมนู
+    @FXML
+    void handleFeature4Btn(ActionEvent event) {
+
+    }
+
+    //ไปหน้าจัดการหอพักจากเมนู
+    @FXML
+    void handleFeature5Btn(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/Models/SqlConnection.java
+++ src/main/java/Models/SqlConnection.java
@@ -1,3 +1,5 @@
+package Models;
+
 import java.sql.*;
 import java.util.ArrayList;
 
